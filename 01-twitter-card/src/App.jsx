4e5bff106da84ecec718e3f74ca6a0e2218f0318
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-01-02 06:03:13 -0500

    feat: modularize the card as a component

--- 01-twitter-card/src/App.jsx
+++ 01-twitter-card/src/App.jsx
@@ -1,19 +1,8 @@
-import './styles/App.css'
+import TwitterFollowCard from './components/TwitterFollowCard'
 
 const App = () => {
   return (
-    <article className='tw-followCard'>
-      <header className='tw-followCard-header'>
-        <img className='tw-followCard-avatar' src="https://unavatar.io/github/alexmarinmendez" alt="" />
-        <div className='tw-followCard-info'>
-          <strong>Alex Marin Mendez</strong>
-          <span className='tw-followCard-infoUserName'>@alexmarinmendez</span>
-        </div>
-      </header>
-      <aside>
-        <button className='tw-followCard-button'>Seguir</button>
-      </aside>
-    </article>
+    <TwitterFollowCard />
   )
 }
 
---
+++ 01-twitter-card/src/components/TwitterFollowCard.jsx
@@ -0,0 +1,20 @@
+import '../styles/TwitterFollowCard.css'
+
+const TwitterFollowCard = () => {
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar' src="https://unavatar.io/github/alexmarinmendez" alt="" />
+                <div className='tw-followCard-info'>
+                    <strong>Alex Marin Mendez</strong>
+                    <span className='tw-followCard-infoUserName'>@alexmarinmendez</span>
+                </div>
+            </header>
+            <aside>
+                <button className='tw-followCard-button'>Seguir</button>
+            </aside>
+        </article>
+    )
+}
+
+export default TwitterFollowCard
