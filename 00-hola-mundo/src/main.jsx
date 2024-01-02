8caf7336e7e38aacda5315b62b879b203aad5d5e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-01-01 22:15:42 -0500

    feat: intro to props

--- 00-hola-mundo/src/main.jsx
+++ 00-hola-mundo/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const createButton = () => {
+const createButton = ({ text }) => {
   return (
-    <button>Me gusta</button>
+    <button>{text}</button>
   )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
-    {createButton()}
-    {createButton()}
+    {createButton({ text: 'Me gusta' })}
+    {createButton({ text: 'Otro me gusta' })}
   </React.Fragment>
 )
