b7bd1fa69226d6e25b80b42209d922f63dffbd84
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-01-02 06:08:59 -0500

    feat: check component's  reusability

--- 01-twitter-card/src/App.jsx
+++ 01-twitter-card/src/App.jsx
@@ -2,7 +2,10 @@ import TwitterFollowCard from './components/TwitterFollowCard'
 
 const App = () => {
   return (
-    <TwitterFollowCard userName='alexmarinmendez' name='Alex Marin Mendez' />
+    <>
+      <TwitterFollowCard userName='alexmarinmendez' name='Alex Marin Mendez' />
+      <TwitterFollowCard userName='facebook' name='Meta' />
+    </>
   )
 }
 
